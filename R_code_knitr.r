61215d27c104e3abcb546510df6b0c3a643c4f0b
boniniselina <80513563+boniniselina@users.noreply.github.com>
2021-04-16 09:32:47 +0200

    Create R_code_knitr.r

---
+++ R_code_knitr.r
@@ -0,0 +1,11 @@
+# R_code_knitr.r
+
+#Imposto la working directory su Windows
+setwd("C:/lab/")
+
+#scaricare il pacchetto knitr
+#install.packages("knitr")
+
+library(knitr) #posso usare anche la funzione require(knitr)
+
+#andiamo a generare un Report da un codice salvato in precedenza che verrà salvato nella cartella precedente
