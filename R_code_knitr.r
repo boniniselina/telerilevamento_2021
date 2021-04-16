c23ca96e431f26c5ab45d0216740f1eb28141a8c
boniniselina <80513563+boniniselina@users.noreply.github.com>
2021-04-16 10:55:34 +0200

    Update R_code_knitr.r

--- R_code_knitr.r
+++ R_code_knitr.r
@@ -9,3 +9,9 @@ setwd("C:/lab/")
 library(knitr) #posso usare anche la funzione require(knitr)
 
 #andiamo a generare un Report da un codice salvato in precedenza che verrà salvato nella cartella precedente
+stitch("C:/lab/R_code_greenland.r", template=system.file("misc", "knitr-template.Rnw", package="knitr"))
+
+#ne uscirà un file.tex (si dice TEC) che avrà lo stesso nome del file di partenza
+#crea anche una cartella con le figure plottate nel codice
+#possiamo trasformarlo in pdf sul sito Overleaf!
+
